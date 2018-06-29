01c47bb7473ea2ec9b702f25b856535676b9395b
Jeff Baucom <jeffbaucom@gmail.com>
2018-06-28 18:21:17 -0700

    style input

--- bango-app/src/Login.js
+++ bango-app/src/Login.js
@@ -6,6 +6,21 @@ import styled from 'styled-components';
 // Login Component
 //=============================================================================
 
+const StyledInput = styled.input`
+    border-radius: 5px;
+    background-color: #fefefe;
+    border-style: solid;
+    box-shadow: none;
+    border: 2px solid #323631;
+    color: #323631;
+    padding: 1.05rem;
+    margin: 0.5rem;
+    font-weight: 600;
+    font-size: 1.5rem;
+    height: 10%;
+    width: 60%;
+`
+
 class Login extends React.Component {
     constructor(props) {
         super(props);
@@ -16,9 +31,9 @@ class Login extends React.Component {
             <div>
                 <form action="">
                     <label for="">Username</label>
-                    <input id="text" type="" placeholder />
+                    <StyledInput id="text" type="" />
                     <label for="">Password</label>
-                    <input id="text" type="" placeholder />
+                    <StyledInput id="text" type="" />
                 </form>
             </div>
         );
