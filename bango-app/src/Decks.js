d50ec655f0d7d6a45c574bca88ec14c079894ddd
Jeff Baucom <jeffbaucom@gmail.com>
2018-09-18 22:15:09 -0700

    add deck listing from server

--- bango-app/src/Decks.js
+++ bango-app/src/Decks.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios';
 
 import styled from 'styled-components';
 
@@ -7,17 +8,33 @@ import styled from 'styled-components';
 //=============================================================================
 
 class Decks extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      decks: []
     }
+  }
 
-    render() {
-        return(
-            <div>
-                <h3>Placeholder for Decks Page</h3>
-            </div>
-        );
-    }
+
+  componentDidMount() {
+    axios.get('/api/base_decks')
+      .then((res) => {
+        const decks = res.data;
+        this.setState({decks});
+      });
+  }
+  
+
+  render() {
+    return(
+      <div>
+        <h3>Placeholder for Decks Page</h3>
+        <ul>
+          { this.state.decks.map(deck => <li>{deck.title}</li>) }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Decks
